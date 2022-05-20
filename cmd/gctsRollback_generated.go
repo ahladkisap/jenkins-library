e431614f37f7913ea73e52dfa76fe8e7f99e7ede
Oliver Feldmann <oliver.feldmann@sap.com>
2022-05-20 14:45:30 +0200

    Re-generate

--- cmd/gctsRollback_generated.go
+++ cmd/gctsRollback_generated.go
@@ -74,7 +74,12 @@ gctsRollback will rollback to the previously active commit in the local reposito
 			}
 
 			if len(GeneralConfig.HookConfig.ANSConfig.ServiceKey) == 0 {
-				GeneralConfig.HookConfig.ANSConfig.ServiceKey = os.Getenv("PIPER_ansServiceKey")
+				// Try ANS hook specific service key
+				GeneralConfig.HookConfig.ANSConfig.ServiceKey = os.Getenv("PIPER_ansHookServiceKey")
+				if len(GeneralConfig.HookConfig.ANSConfig.ServiceKey) > 0 {
+					// Try ANS service key from step implementation
+					GeneralConfig.HookConfig.ANSConfig.ServiceKey = os.Getenv("PIPER_ansServiceKey")
+				}
 			}
 			if len(GeneralConfig.HookConfig.ANSConfig.ServiceKey) > 0 {
 				log.RegisterSecret(GeneralConfig.HookConfig.ANSConfig.ServiceKey)
